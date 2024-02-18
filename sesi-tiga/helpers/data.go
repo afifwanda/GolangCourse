4f474dda80ecf6af7e606f6994cf3ccce820c4cd
Afif Julio <54105946+afifwanda@users.noreply.github.com>
2024-02-18 19:40:54 +0700

    separate data from functions

---
+++ sesi-tiga/helpers/data.go
@@ -0,0 +1,8 @@
+package helpers
+
+var people = []Person{
+	{Id: 1, Nama: "Jane", Alamat: "Johannesburg", Pekerjaan: "Bartender", Alasan: "Switch Career"},
+	{Id: 2, Nama: "Doe", Alamat: "Medan", Pekerjaan: "Pelajar", Alasan: "Menambah Ilmu"},
+	{Id: 3, Nama: "William", Alamat: "Detroit", Pekerjaan: "Guru", Alasan: "Menambah Ilmu"},
+	{Id: 4, Nama: "Dafoe", Alamat: "Lahore", Pekerjaan: "Mahasiswa", Alasan: "Menambah referensi"},
+}
