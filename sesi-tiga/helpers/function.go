06b0e49922c4de7f5f18c5f8d8b28b3fb53ad765
Afif Julio <54105946+afifwanda@users.noreply.github.com>
2024-02-18 19:41:17 +0700

    Update function.go

--- sesi-tiga/helpers/function.go
+++ sesi-tiga/helpers/function.go
@@ -1,12 +1,5 @@
 package helpers
 
-var people = []Person{
-	{Id: 1, Nama: "Jane", Alamat: "Johannesburg", Pekerjaan: "Bartender", Alasan: "Switch Career"},
-	{Id: 2, Nama: "Doe", Alamat: "Medan", Pekerjaan: "Pelajar", Alasan: "Menambah Ilmu"},
-	{Id: 3, Nama: "William", Alamat: "Detroit", Pekerjaan: "Guru", Alasan: "Menambah Ilmu"},
-	{Id: 4, Nama: "Dafoe", Alamat: "Lahore", Pekerjaan: "Mahasiswa", Alasan: "Menambah referensi"},
-}
-
 func FindPersonById(id interface{}) *Person {
 	for _, p := range people {
 		if p.Id == id {
